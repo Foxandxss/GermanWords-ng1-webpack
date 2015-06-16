2d7d30b910770d88487cba469bad7d5715cca1db
Jesus Rodriguez <foxandxss@gmail.com>
2015-06-16 14:07:37 +0200

    Refactor webpack config to allow testing and prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,7 @@
-'use strict';
-
-module.exports = {
-  entry: {
-    app: ['./src/app.js']
-  },
-  output: {
-    path: './src',
-    filename: 'bundle.js'
-  },
-  devtool: 'source-map',
-  devServer: {
-    historyApiFallback: true
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        loader: 'style!css'
-      },
-      {
-        test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader?name=res/[name].[ext]?[hash]'
-      },
-      {
-        test: /\.html$/,
-        loader: 'raw'
-      }
-    ]
-  }
-};
+/**
+ * Webpack config for development
+ */
+module.exports = require('./webpack.make')({
+  BUILD: false,
+  TEST: false
+});
---
+++ webpack.make.js
@@ -0,0 +1,147 @@
+'use strict';
+
+// Modules
+var autoprefixer = require('autoprefixer-core');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = function makeWebpackConfig (options) {
+  /**
+   * Environment type
+   * BUILD is for generating minified builds
+   * TEST is for generating test builds
+   */
+  var BUILD = !!options.BUILD;
+  var TEST = !!options.TEST;
+
+  /**
+   * Config
+   * Reference: http://webpack.github.io/docs/configuration.html
+   * This is the object where all configuration gets set
+   */
+  var config = {};
+
+  /**
+   * Entry
+   * Reference: http://webpack.github.io/docs/configuration.html#entry
+   * Should be an empty object if it's generating a test build
+   * Karma will set this when it's a test build
+   */
+  if (TEST) {
+    config.entry = {}
+  } else {
+    config.entry = {
+      app: './src/app.js'
+    }
+  }
+
+  /**
+   * Output
+   * Reference: http://webpack.github.io/docs/configuration.html#output
+   * Should be an empty object if it's generating a test build
+   * Karma will handle setting it up for you when it's a test build
+   */
+  if (TEST) {
+    config.output = {}
+  } else {
+    config.output = {
+      // Absolute output directory
+      path: __dirname + '/src',
+
+      // Output path from the view of the page
+      // Uses webpack-dev-server in development
+      publicPath: BUILD ? '/' : 'http://localhost:8080/',
+
+      // Filename for entry points
+      // Only adds hash in build mode
+      filename: BUILD ? '[name].[hash].js' : '[name].bundle.js',
+
+      // Filename for non-entry points
+      // Only adds hash in build mode
+      chunkFilename: BUILD ? '[name].[hash].js' : '[name].bundle.js'
+    }
+  }
+
+  /**
+   * Devtool
+   * Reference: http://webpack.github.io/docs/configuration.html#devtool
+   * Type of sourcemap to use per build type
+   */
+  if (TEST) {
+    config.devtool = 'inline-source-map';
+  } else if (BUILD) {
+    config.devtool = 'source-map';
+  } else {
+    config.devtool = 'eval';
+  }
+
+  /**
+   * Loaders
+   * Reference: http://webpack.github.io/docs/configuration.html#module-loaders
+   * List: http://webpack.github.io/docs/list-of-loaders.html
+   * This handles most of the magic responsible for converting modules
+   */
+
+  // Initialize module
+  config.module = {
+    preLoaders: [],
+    loaders: [{
+      // JS LOADER
+      // Reference: https://github.com/babel/babel-loader
+      // Transpile .js files using babel-loader
+      // Compiles ES6 and ES7 into ES5 code
+      test: /\.js$/,
+      loader: 'babel?optional=runtime',
+      exclude: /node_modules/
+    }, {
+      // ASSET LOADER
+      // Reference: https://github.com/webpack/file-loader
+      // Copy png, jpg, jpeg, gif, svg, woff, ttf, eot files to output
+      // Rename the file using the asset hash
+      // Pass along the updated reference to your code
+      // You can add here any file extension you want to get copied to your output
+      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+      loader: 'file'
+    }, {
+      // HTML LOADER
+      // Reference: https://github.com/webpack/raw-loader
+      // Allow loading html through js
+      test: /\.html$/,
+      loader: 'raw'
+    }]
+  };
+
+  // CSS LOADER
+  // Reference: https://github.com/webpack/css-loader
+  // Allow loading css through js
+  //
+  // Reference: https://github.com/postcss/postcss-loader
+  // Postprocess your css with PostCSS plugins
+  var cssLoader = {
+    test: /\.css$/,
+    // Reference: https://github.com/webpack/extract-text-webpack-plugin
+    // Extract css files in production builds
+    //
+    // Reference: https://github.com/webpack/style-loader
+    // Use style-loader in development for hot-loading
+    loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+  };
+
+  // Add cssLoader to the loader list
+  config.module.loaders.push(cssLoader);
+
+  /**
+   * Plugins
+   * Reference: http://webpack.github.io/docs/configuration.html#plugins
+   * List: http://webpack.github.io/docs/list-of-plugins.html
+   */
+  config.plugins = [
+    // Reference: https://github.com/webpack/extract-text-webpack-plugin
+    // Extract css files
+    // Disabled when in test mode or not in build mode
+    new ExtractTextPlugin('[name].[hash].css', {
+      disable: !BUILD || TEST
+    })
+  ];
+
+  return config;
+};
