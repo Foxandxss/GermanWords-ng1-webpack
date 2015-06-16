92c204bd954736824a69259051d83e8e47271c39
Jesus Rodriguez <foxandxss@gmail.com>
2015-06-16 19:51:18 +0200

    Finish tests

--- src/services/auth.service.test.js
+++ src/services/auth.service.test.js
@@ -1,9 +1,58 @@
-import auth from './auth.service';
+import authModule from './auth.service';
 
 describe('Service: auth', function() {
-  beforeEach(angular.mock.module(auth.name));
+  var $http, $q, $rootScope, auth;
 
-  beforeEach(angular.mock.inject(function() {
-    
+  beforeEach(angular.mock.module(authModule.name));
+
+  beforeEach(angular.mock.inject(function(_$http_, _$q_, _$rootScope_, _auth_) {
+    $http = _$http_;
+    $q = _$q_;
+    $rootScope = _$rootScope_;
+    auth = _auth_;
   }));
+
+  it('#isAuth will return a boolean based on jwt token', function() {
+    auth.token = 'foo';
+
+    expect(auth.isAuth()).toBeTruthy();
+
+    auth.token = null;
+
+    expect(auth.isAuth()).toBeFalsy();
+  });
+
+  it('#getUser returns a decoded user', function() {
+    auth.user = 'someuser';
+
+    expect(auth.getUser()).toBe('someuser');
+  });
+
+  it('#login puts a new token in localStorage', function() {
+    var res = {
+      data: {
+        id_token: 'foo'
+      }
+    };
+
+    spyOn($http, 'post').and.returnValue($q.when(res));
+    spyOn(localStorage, 'setItem');
+
+    auth.login('user', 'pass');
+
+    $rootScope.$digest();
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('jwt', 'foo');
+    expect(auth.token).toBe('foo');
+  });
+
+  it('#logout reset everything', function() {
+    spyOn(localStorage, 'removeItem');
+
+    auth.logout();
+
+    expect(localStorage.removeItem).toHaveBeenCalledWith('jwt');
+    expect(auth.token).toBeNull();
+    expect(auth.user).toBeNull();
+  });
 });
---
+++ src/services/words.service.test.js
@@ -0,0 +1,32 @@
+import wordsModule from './words.service';
+
+describe('Service: auth', function() {
+  var $httpBackend, $q, $rootScope, words;
+
+  beforeEach(angular.mock.module(wordsModule.name));
+
+  beforeEach(angular.mock.inject(function(_$httpBackend_, _$q_, _$rootScope_, _words_) {
+    $httpBackend = _$httpBackend_;
+    $q = _$q_;
+    $rootScope = _$rootScope_;
+    words = _words_;
+  }));
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('gets a random word with #getWords()', function() {
+    var res = {
+      data: {
+        german: 'Haus'
+      }
+    };
+    $httpBackend.expectGET('http://localhost:3001/api/random-word').respond(200, res);
+
+    words.getWords();
+
+    $httpBackend.flush();
+  });
+});
